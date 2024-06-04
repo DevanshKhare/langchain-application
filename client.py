ecf273b65f032ce4ee8301925722d46d1d3e7fb7
Devansh Khare <devanshkhare0@gmail.com>
2024-06-04 12:12:45 +0530

    basic streamlit setup

--- client.py
+++ client.py
@@ -1,5 +1,15 @@
 import requests
 import streamlit as st
 
+def get_openai_response(input):
+    response = requests.post(
+        "http://localhost:8000/essay/invoke",
+        json={"input": {"topic": input}}
+    )
+    return response.json()["output"]["content"]
+
 st.title("AI Sensing")
 input_text = st.text_input("Write an essay on the topic you want")
+
+if input_text:
+    st.write(get_openai_response(input_text))
